836bd890a2a44b96de1faeca68b1757638f23421
wooxiuyu <xiuyuw2021@gmail.com>
2026-04-06 15:52:09 +0800

    third commit

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,10 +2,10 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); //comment
+        System.out.println("Hello, students!"); //comment
     }
 
     public String getGreeting() {
-        return "Bello, World!";
+        return "Hello, World!";
     }
 }
